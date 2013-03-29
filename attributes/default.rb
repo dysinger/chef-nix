d94a3069d9341a9c5bb90fafa3964f1d04c128dc
Tim Dysinger <tim@knewton.com>
2013-03-28 18:44:39 -1000

    nix 1.5.1 as a Upstart service
    Change-Id: I40568f76dc7c9dfcd1998cd5fc0015440a605320

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,3 @@
-default['nix']['version'] = '1.3'
+default['nix']['version'] = '1.5.1'
+default['nix']['url']     =
+  "http://hydra.nixos.org/build/4253979/download/1/nix-1.5.1-x86_64-linux.tar.bz2"
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,17 +5,85 @@
 # Copyright 2012, Knewton, Apache 2.0
 #
 
-key = "nix-#{node['nix']['version']}" +
-  "-#{node['platform']}-#{node['platform_version']}" + 
-  "-#{node['kernel']['machine']}.tar.bz2"
+# NIX INSTALL
 
-env = '/nix/var/nix/profiles/default/etc/profile.d/nix.sh'
+bash 'nix install' do
+  code "wget -O- #{node['nix']['url']}|tar xj -C /"
+  creates '/nix/store'
+end
 
-bash 'download & extract nix' do
-  code    "s3-get -e stack_iam -b knewton-utility-build #{key}|tar oxj -C /"
-  creates env
+bash 'nix finish install' do
+  cwd '/root'
+  code 'sudo -i /usr/bin/nix-finish-install'
+  creates '/nix/var/nix/profiles/default/etc/profile.d/nix.sh'
 end
 
 link '/etc/profile.d/nix.sh' do
-  to env
+  to '/nix/var/nix/profiles/default/etc/profile.d/nix.sh'
+end
+
+# SHELL & BUILD GROUP & USERS FOR NIX
+
+execute 'echo "/bin/false"|tee -a /etc/shells' do
+  not_if 'grep false /etc/shells'
+end
+
+(1..10).each do |i|
+  user "nixbld#{i}" do
+    system true
+    group 'nogroup'
+    home  '/var/empty'
+    shell '/bin/false'
+  end
+end
+
+group 'nixbld' do
+  system true
+  members (1..10).collect {|i| "nixbld#{i}"}
+end
+
+# FOLDER PERMISSIONS
+
+directory '/nix/store' do
+  recursive true
+  owner 'root'
+  group 'nixbld'
+  mode  '1775'
+end
+
+directory '/nix/var/nix/profiles' do
+  recursive true
+  owner 'root'
+  group 'root'
+  mode  '1777'
+end
+
+directory '/nix/var/nix/profiles/per-user' do
+  recursive true
+  owner 'root'
+  group 'root'
+  mode  '1777'
+end
+
+# NIX DAEMON
+
+directory '/etc/nix'
+
+file '/etc/nix/nix.conf' do
+  content 'build-users-group = nixbld'
+end
+
+file '/etc/profile.d/nix-worker.sh' do
+  content 'export NIX_REMOTE=daemon'
+end
+
+file '/etc/profile.d/nix-defexpr.sh' do
+  content '[ ! -e $HOME/.nix-defexpr ] && mkdir $HOME/.nix-defexpr'
+end
+
+cookbook_file '/etc/init/nix-worker.conf'
+
+service 'nix-worker' do
+  provider Chef::Provider::Service::Upstart
+  action [ :start ]
 end
