2122a8bcb4c0e6c5adcc1dba5abe57fd96bd4e86
Tim Dysinger <tim@knewton.com>
2013-05-20 18:35:06 -1000

    updated metadata
    Change-Id: I9ae55133ae24fa843bdde5cfc6cedae99185919d

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,21 @@
+# Copyright 2013, Knewton, Apache 2.0 License
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed
+# with this work for additional information regarding copyright
+# ownership.  The ASF licenses this file to you under the Apache
+# License, Version 2.0 (the "License"); you may not use this file
+# except in compliance with the License.  You may obtain a copy of the
+# License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.  See the License for the specific language governing
+# permissions and limitations under the License.
+
 default['nix']['version'] = '1.5.1'
 default['nix']['url']     =
   "http://hydra.nixos.org/build/4253979/download/1/nix-1.5.1-x86_64-linux.tar.bz2"
--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,26 @@
-maintainer       "Knewton Systems Engineering"
-maintainer_email "se@knewton.com"
-license          "Apache 2.0"
-supports         "ubuntu"
+# Copyright 2013, Knewton, Apache 2.0 License
 
-name        "nix"
-description "Installs/Configures Nix"
-version     "1.0.201301161610"
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed
+# with this work for additional information regarding copyright
+# ownership.  The ASF licenses this file to you under the Apache
+# License, Version 2.0 (the "License"); you may not use this file
+# except in compliance with the License.  You may obtain a copy of the
+# License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.  See the License for the specific language governing
+# permissions and limitations under the License.
+
+maintainer       'Knewton Systems Engineering'
+maintainer_email 'se@knewton.com'
+license          'Apache2'
+supports         'ubuntu'
+
+name        'nix'
+description 'Installs/Configures Nix'
+version     '1.5.2'
